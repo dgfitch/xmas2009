7df04c8ce9dfad80cae7688d1cd46b05a18b8d68
Dan Fitch <dan@rapt.us>
2009-12-02 20:36:33 -0600

    Trying to figure out how to make static bodies in 0.6, durp

--- love/objects/SimpleRect.lua
+++ love/objects/SimpleRect.lua
@@ -1,18 +1,29 @@
 require "objects/DrawablePoly.lua"
 
 SimpleRect = {
-  load = function( world, x, y )
+  load = function( world, x, y, width, height, color, static )
     local self = {}
     mixin( self, SimpleRect )
     mixin( self, DrawablePoly )
     self.body = love.physics.newBody( world, x, y, 0.5, 10 )
-    self.size = math.random(60) + 20
-    if math.random() < 0.5 then
+
+    ---- this is wrong
+    --if static then self.body:setStatic() end
+
+    local size = math.random(60) + 20
+    if not width then width = size end
+    if not height then height = size end
+
+    if color then
+      self.color = color
+    elseif math.random() < 0.5 then
       self.color = { math.random(255), 0, 0 }
     else
       self.color = { 0, math.random(255), 0 }
     end
-    self.poly = love.physics.newRectangleShape( self.body, 0, 0, self.size, self.size, math.random() * math.twopi )
+
+    self.poly = love.physics.newRectangleShape( self.body, 0, 0, width, height, math.random() * math.twopi )
+    self.body:setMassFromShapes()
     return self
   end,
 }
--- love/states/game.lua
+++ love/states/game.lua
@@ -1,9 +1,14 @@
+-- a machine gun that shoots presents
 states.game = {
+  wallColor = {100,100,100},
+
   initialize = function(s)
     s.objects = {}
 
     s.world = love.physics.newWorld( WIDTH, HEIGHT )
     s.world:setGravity( 0, 200 )
+
+    s:addWall( 100, 500, 500, 20 )
   end,
 
   draw = function(s)
@@ -16,7 +21,7 @@ states.game = {
 
   update = function(s, dt)
     s.world:update( dt )
-    if love.mouse.isDown( 'l' ) then
+    if love.mouse.isDown( 'r' ) then
       if math.random() < 0.2 then
         s:addPoly( love.mouse.getX(), love.mouse.getY() )
       else
@@ -33,6 +38,11 @@ states.game = {
   addPoly = function(s, x, y)
     local r = PolyTest.load( s.world, x, y )
     table.insert( s.objects, r )
+  end,
+
+  addWall = function(s, x, y, w, h)
+    local w = SimpleRect.load( s.world, x, y, w, h, s.wallColor, true )
+    table.insert( s.objects, w )
   end,
 }
 
