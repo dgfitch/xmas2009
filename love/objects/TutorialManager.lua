e3fb7f258507e27eb7ae8bd36beb2990efdcc9cb
Dan Fitch <dgfitch@gmail.com>
2009-12-21 19:34:11 -0600

    Fixing compilation bug, but having problems running on Windows build 20091123 now

--- love/objects/TutorialManager.lua
+++ love/objects/TutorialManager.lua
@@ -4,31 +4,31 @@ local t = Tutorial.load
 
 Tutorials = {
   t({
-    text = "This is a Present Gun. It fires presents!"
+    text = "This is a Present Gun. It fires presents!",
     trigger = function(self, object)
       return object:kindOf(MachineGun)
     end,
   }),
   t({
-    text = "Help Santa send these Good Presents by moving them to the Goal."
+    text = "Help Santa send these Good Presents by moving them to the Goal.",
     trigger = function(self, object)
       return object:kindOf(Present) and not object.broken
     end,
   }),
   t({
-    text = "Stop delivery of Bad Presents by moving them out of the goal area, or right click to destroy them."
+    text = "Stop delivery of Bad Presents by moving them out of the goal area, or right click to destroy them.",
     trigger = function(self, object)
       return object:kindOf(Present) and object.broken
     end,
   }),
   t({
-    text = "This is Coal. It pops out of packages if you destroy good ones or even bad ones inside the Goal."
+    text = "This is Coal. It pops out of packages if you destroy good ones or even bad ones inside the Goal.",
     trigger = function(self, object)
       return object:kindOf(Coal)
     end,
   }),
   t({
-    text = "This is a Wall. Uh, that should be self explanatory, right?"
+    text = "This is a Wall. Uh, that should be self explanatory, right?",
     trigger = function(self, object)
       return object:kindOf(Wall)
     end,
